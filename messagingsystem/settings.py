a13e655f642d8fd952104b9e05ae8c1e360e9c0d
OrMeirchak <ormeirchak@gmail.com>
2022-08-03 13:42:32 +0300

    updated settings 2

--- messagingsystem/settings.py
+++ messagingsystem/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-^qs%5*m7e_r^^+n&0)n%r+-ye2dz07tuj5tlq25!yugdzj1o11'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-^qs%5*m7e_r^^+n&0)n%r+-ye2dz07tuj5tlq25!yugdzj1o11'
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -30,7 +30,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'kubernetes.docker.internal',
     '127.0.0.1',
-    'messagingsystemapp.herokuapp.com'
+    'messagingsystemabraapp.herokuapp.com'
 ]
 
 
